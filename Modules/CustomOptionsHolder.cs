0a55c3510881da37818acc3f097df9d8c0da2904
Ape <155788660+ApeMV@users.noreply.github.com>
2025-12-05 22:37:51 +0100

    Add custom options menu

---
+++ Modules/CustomOptionsHolder.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+using System.Collections.Generic;
+
+// https://github.com/tukasa0001/TownOfHost/blob/main/Modules/OptionHolder.cs
+namespace HNSRevamped
+{
+    [HarmonyPatch]
+    public static class Options
+    {
+        static Task taskOptionsLoad;
+
+        [HarmonyPatch(typeof(TranslationController), nameof(TranslationController.Initialize)), HarmonyPostfix]
+        public static void OptionsLoadStart()
+        {
+            taskOptionsLoad = Task.Run(Load);
+        }
+
+        [HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start)), HarmonyPostfix]
+        public static void WaitOptionsLoad()
+        {
+            taskOptionsLoad.Wait();
+        }
+
+        public static OptionItem Testy;
+        public static OptionItem Testy1;
+        public static OptionItem Testy2;
+
+
+        public static bool IsLoaded = false;
+
+        public static void Load()
+        {
+            if (IsLoaded) return;
+
+            //Main settings
+            Testy = IntegerOptionItem.Create(3, "Testy", new IntegerValueRule(0, 50, 5), 10, TabGroup.GameModifiers, false);
+            Testy1 = BooleanOptionItem.Create(4, "Testy1", true, TabGroup.ModSettings, false)
+                .SetColor(Color.red);
+            Testy2 = BooleanOptionItem.Create(5, "Testy2", true, TabGroup.ModSettings, false)
+                .SetParent(Testy1);
+            IsLoaded = true;
+        }
+    }
+}
--- Modules/Utils.cs
+++ Modules/Utils.cs
@@ -1,8 +1,91 @@
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using AmongUs.GameOptions;
+using Hazel;
+using UnityEngine;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
+using InnerNet;
+using System.IO;
+using System.Reflection;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using AmongUs.InnerNet.GameDataMessages;
+
+using Object = UnityEngine.Object;
 
 namespace HNSRevamped;
 
-public static class Utils
-{
-    public static bool isHideNSeek => GameOptionsManager.Instance.CurrentGameOptions.GameMode == GameModes.HideNSeek;
-}
+    public static class Utils
+    {
+        public static bool isHideNSeek => GameOptionsManager.Instance.CurrentGameOptions.GameMode == GameModes.HideNSeek;
+
+        public static string ColorString(Color32 color, string str) => $"<#{color.r:x2}{color.g:x2}{color.b:x2}{color.a:x2}>{str}</color>";
+        public static string ColorToHex(Color32 color) => $"#{color.r:x2}{color.g:x2}{color.b:x2}{color.a:x2}";
+
+
+
+        public static string GetTabName(TabGroup tab)
+        {
+            switch (tab)
+            {
+                case TabGroup.ModSettings:
+                    return "Mod Settings";
+                case TabGroup.GameModifiers:
+                    return "Game Modifiers";
+
+                default:
+                    return "";
+            }
+        }
+
+        public static void DestroyTranslator(this GameObject obj)
+        {
+            var translator = obj.GetComponent<TextTranslatorTMP>();
+            if (translator != null)
+            {
+                Object.Destroy(translator);
+            }
+        }
+
+        public static void DestroyTranslator(this MonoBehaviour obj) => obj.gameObject.DestroyTranslator();
+
+        public static void CustomSettingsChangeMessageLogic(this NotificationPopper notificationPopper, OptionItem optionItem, string text, bool playSound)
+        {
+            if (notificationPopper.lastMessageKey == 10000 + optionItem.Id && notificationPopper.activeMessages.Count > 0)
+            {
+                notificationPopper.activeMessages[notificationPopper.activeMessages.Count - 1].UpdateMessage(text);
+            }
+            else
+            {
+                notificationPopper.lastMessageKey = 10000 + optionItem.Id;
+                LobbyNotificationMessage settingmessage = Object.Instantiate(notificationPopper.notificationMessageOrigin, Vector3.zero, Quaternion.identity, notificationPopper.transform);
+                settingmessage.transform.localPosition = new Vector3(0f, 0f, -2f);
+                settingmessage.SetUp(text, notificationPopper.settingsChangeSprite, notificationPopper.settingsChangeColor, new Action(() =>
+                {
+                    notificationPopper.OnMessageDestroy(settingmessage);
+                }));
+                notificationPopper.ShiftMessages();
+                notificationPopper.AddMessageToQueue(settingmessage);
+            }
+            if (playSound)
+            {
+                SoundManager.Instance.PlaySoundImmediate(notificationPopper.settingsChangeSound, false, 1f, 1f, null);
+            }
+        }
+
+        public static string GetOptionNameSCM(this OptionItem optionItem)
+        {
+            if (optionItem.Name == "Enable")
+            {
+                int id = optionItem.Id;
+                while (id % 10 != 0)
+                    --id;
+                var optionItem2 = OptionItem.AllOptions.FirstOrDefault(opt => opt.Id == id);
+                return optionItem2 != null ? optionItem2.GetName() : optionItem.GetName();
+            }
+            else
+                return optionItem.GetName();
+        }
+    }
